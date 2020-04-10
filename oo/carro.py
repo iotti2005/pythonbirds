cc96e871a85c741215628742ab4b4e1d507cc601
iotti2005 <guilhermeiotti2005@gmail.com>
2020-04-10 14:27:00 -0300

    velocidade terminada(mesmo)

--- oo/carro.py
+++ oo/carro.py
@@ -1,12 +1,13 @@
 class Carro:
-    def __init__(self, *velocidade, aceleracao=None, freagem = None):
+    def __init__(self, velocidade= None, aceleracao=None, freagem=None):
         self.aceleracao = aceleracao
         self.freagem = freagem
-        self.velocidade = list(velocidade)
+        self.velocidade = velocidade
 
-
-if __name__ == '__main__':
+if __name__ == 'carro':
     calculodavelocidade1 = Carro(aceleracao=10)
-    calculodavelocidade2 = Carro(freagem=3)
-    velocidade=((calculodavelocidade1.aceleracao)-(calculodavelocidade2.freagem))
+    calculodavelocidade2 = Carro(freagem=20)
+    velocidade = ((calculodavelocidade1.aceleracao) - (calculodavelocidade2.freagem))
+    if velocidade <(-1):
+        velocidade = 0
     print(velocidade)
